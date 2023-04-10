35e3c1fefc6fed9a1e834f6b7cd58a8d97b889c9
Declan Keighley <dec.keighley@gmail.com>
2023-04-10 20:50:11 +1200

    hji

--- src/scenes/Core.tsx
+++ src/scenes/Core.tsx
@@ -5,10 +5,11 @@ export function Core() {
   const ref = useRef<THREE.Mesh>(null!);
 
   useFrame((state, delta) => (ref.current.rotation.x += delta));
+  useFrame((state, delta) => (ref.current.rotation.y += delta));
 
   return (
     <mesh ref={ref}>
-      <octahedronGeometry args={[2, 2]} />
+      <octahedronGeometry args={[3, 2]} />
       <meshStandardMaterial />
     </mesh>
   );
--- src/scenes/RootScene.tsx
+++ src/scenes/RootScene.tsx
@@ -3,9 +3,10 @@ import React from "react";
 
 export function RootScene(props: any) {
   return (
-    <Canvas>
+    <Canvas shadows={"soft"} style={{ height: "100vh" }}>
       <ambientLight intensity={0.1} />
       <directionalLight color="red" position={[0, 0, 5]} />
+      <camera position={[50, 0, 0]} />
       {props.children}
     </Canvas>
   );
